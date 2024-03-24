ef2d7d3cdf6c8fd80dab5f5f68d44287e82cf29c
ishashriya <isha.shriya3@gmail.com>
2024-03-24 14:08:25 +1100

    version 1.4

--- eduprod/urls.py
+++ eduprod/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path ('home3-4', views.home34, name='home3-4'),
+    path ('home5-6', views.home56, name='home5-6'),
+    path ('home7-8', views.home78, name='home7-8')
 ]
+
