1711c39251beb1902754a1b571a09ddfb6a01c5c
ishashriya <isha.shriya3@gmail.com>
2024-03-26 18:20:02 +1100

    version 1.6 with errors via checklist

--- eduprod/models.py
+++ eduprod/models.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    answer_text = models.CharField(max_length=200)
+    topics = [
+        ('Addition', '+'),
+        ('Subtraction', '-')
+    ]
+
+
+    subject = models.CharField(max_length=200, choices=topics, default='General')
+    question_text = models.CharField(max_length=200, default='')
+    answer_text = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.question_text
