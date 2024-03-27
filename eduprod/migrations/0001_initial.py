ae1bc5295f2eac3dadf70211d905c695136a398e
ishashriya <isha.shriya3@gmail.com>
2024-03-27 11:45:44 +1100

    version 1.6.3 with bugs

--- eduprod/migrations/0001_initial.py
+++ eduprod/migrations/0001_initial.py
@@ -3,6 +3,7 @@
 from django.db import migrations, models
 
 
+
 class Migration(migrations.Migration):
 
     initial = True
@@ -18,5 +19,28 @@ class Migration(migrations.Migration):
                 ('question_text', models.CharField(max_length=200)),
                 ('answer_text', models.TextField()),
             ],
+            def set_default_age(apps, schema_editor):
+                Question = apps.get_model('yourappname', 'Question')
+                Question.objects.filter(age='').update(age=3)  # Set default value for existing records
+
+            class Migration(migrations.Migration):
+
+                dependencies = [
+                  ('yourappname', 'previous_migration'),
+                         ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='age',
+            field=models.IntegerField(choices=[(3, '3 years old'), (4, '4 years old'), (5, '5 years old'), (6, '6 years old'), (7, '7 years old'), (8, '8 years old')], default=3),
+            preserve_default=False,  # Remove this line if using a default value
         ),
+        migrations.RunPython(set_default_age),
+    ]
+
+        ),
+    
+
+
     ]
--- eduprod/models.py
+++ eduprod/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.db import migrations, models
+
 
 class Question(models.Model):
     topics = [
@@ -16,10 +18,32 @@ class Question(models.Model):
         (8, '8 years old')
     ]
 
-    age = models.IntegerField(choices=AGE_CHOICES, default='General')  # Default age set to 3 years old
+    age = models.IntegerField(choices=AGE_CHOICES, default='')  # Default age set to 3 years old
     subject = models.CharField(max_length=200, choices=topics, default='General')
     question_text = models.CharField(max_length=200, default='')
     answer_text = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.question_text
+    
+    from django.db import migrations, models
+
+def set_default_age(apps, schema_editor):
+    Question = apps.get_model('yourappname', 'Question')
+    Question.objects.filter(age='').update(age=3)  # Set default value for existing records
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('yourappname', 'previous_migration'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='age',
+            field=models.IntegerField(choices=[(3, '3 years old'), (4, '4 years old'), (5, '5 years old'), (6, '6 years old'), (7, '7 years old'), (8, '8 years old')], default=3),
+            preserve_default=False,  # Remove this line if using a default value
+        ),
+        migrations.RunPython(set_default_age),
+    ]
