0293731a66d81e976da6026aebd2c430255f41e7
ishashriya <isha.shriya3@gmail.com>
2024-03-28 22:41:56 +1100

    1.6.5

--- eduprod/urls.py
+++ eduprod/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'eduprod'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('get_questions/', views.get_questions, name='get_questions'),
--- eduprod/views.py
+++ eduprod/views.py
@@ -1,19 +1,16 @@
-from django.core import serializers
 from django.shortcuts import render
 from .models import Question
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import JsonResponse
-
 
 @login_required(login_url='/accounts/signup')
 def home(request):
-    return render(request, 'eduprod/home.html')
+    selected_age = request.GET.get('age')  # Get the selected age from the request
+    return render(request, 'eduprod/home.html', {'selected_age': selected_age})
 
 @login_required(login_url='/accounts/signup')
 def get_questions(request):
     topic = request.GET.get('topic')
-    questions = Question.objects.filter(topic=topic)  # Assuming 'topic' is a field in your Question model
+    questions = Question.objects.filter(subject=topic)
     data = [{'question_text': question.question_text} for question in questions]
     return JsonResponse(data, safe=False)
