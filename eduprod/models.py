8f0756c9269252c6b92932085fff464aa78c01e4
ishashriya <isha.shriya3@gmail.com>
2024-03-31 17:19:46 +1100

    version 1.7 (questions)

--- eduprod/models.py
+++ eduprod/models.py
@@ -3,6 +3,168 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.db import migrations, models
 
+class AnswerChoice:
+    def __init__(self, answerChoice1, answerChoice2, answerChoice3, answerChoice4):
+        self.answerChoice1 = answerChoice1
+        self.answerChoice2 = answerChoice2
+        self.answerChoice3 = answerChoice3
+        self.answerChoice4 = answerChoice4
+
+        @property
+        def answerChoice1(self):
+            return self.answerChoice1
+        
+        @answerChoice1.setter
+        def answerChoice1(self, value):
+            self.answerChoice1 = value
+
+        @property
+        def answerChoice2(self):
+            return self.answerChoice2
+        
+        @answerChoice2.setter
+        def answerChoice2(self, value):
+            self.answerChoice2 = value
+
+        @property
+        def answerChoice3(self):
+            return self.answerChoice3
+        
+        @answerChoice3.setter
+        def answerChoice3(self, value):
+            self.answerChoice3 = value
+
+        @property
+        def answerChoice4(self):
+            return self.answerChoice4
+        
+        @answerChoice4.setter
+        def answerChoice4(self, value):
+            self.answerChoice4 = value
+        
+
+class UserQuestion1:
+    def __init__(self, id, questionText, answerChoice1, answerChoice2, answerChoice3, answerChoice4, answer):
+        self.id = id
+        self.questionText = questionText
+        self.answerChoice1 = answerChoice1
+        self.answerChoice2 = answerChoice2
+        self.answerChoice3 = answerChoice3
+        self.answerChoice4 = answerChoice4
+        self.answer = answer
+        
+        @property
+        def id(self):
+            return self.id
+        
+        @id.setter
+        def id(self, value):
+            self.id = value
+
+        @property
+        def questionText(self):
+            return self.questionText
+        
+        @questionText.setter
+        def questionText(self, value):
+            self.questionText = value  
+
+        @property
+        def answerChoice1(self):
+            return self.answerChoice1
+        
+        @answerChoice1.setter
+        def answerChoice1(self, value):
+            self.answerChoice1 = value
+
+        @property
+        def answerChoice2(self):
+            return self.answerChoice2
+        
+        @answerChoice2.setter
+        def answerChoice2(self, value):
+            self.answerChoice2 = value
+
+        @property
+        def answerChoice3(self):
+            return self.answerChoice3
+        
+        @answerChoice3.setter
+        def answerChoice3(self, value):
+            self.answerChoice3 = value
+
+        @property
+        def answerChoice4(self):
+            return self.answerChoice4
+        
+        @answerChoice4.setter
+        def answerChoice4(self, value):
+            self.answerChoice4 = value
+
+        @property
+        def answer(self):
+            return self.answer
+        
+        @answer.setter
+        def answer(self, value):
+            self.answer = value
+
+
+class UserQuestion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    question_text = models.CharField(max_length=255)
+    answer_choice1 = models.CharField(max_length=255)
+    answer_choice2 = models.CharField(max_length=255)
+    answer_choice3 = models.CharField(max_length=255)
+    answer_choice4 = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+    
+
+class QuestionManager:
+    def __init__(self):
+            self.questions = []  # list of question objects
+    
+    def createQuestion1():
+        q1 = UserQuestion(1, "Question 1 Square", "circle", "triangle", "square", "rectangle", "square") # Create a question with 5 choices
+        return q1
+    
+    def createQuestion2():
+        q2 = UserQuestion(2, "Question 2 Circle", "circle", "triangle", "square", "rectangle", "circle") # Create a question with 5 choices
+        return q2
+
+    def createQuestion3():
+        q3 = UserQuestion(3, "Question 3 Rectangle", "circle", "triangle", "square", "rectangle", "rectangle") # Create a question with 5 choices
+        return q3
+    # Create question 2 and set its attributes using the function above
+    
+    questions = [createQuestion1(), createQuestion2(),createQuestion3()]  
+
+
+
+#         @property
+#         def is_answered(self):
+#             return bool(self.answer)
+            
+#     answer = models.ForeignKey('Answer', on_delete=models.SET_NULL, blank=True, null=True, related_name='+')
+    
+# class QuestionManager(models.Manager):
+#     def add_question(self, text, author):
+#         q = self.model()
+#         q.text = text
+#         q.author = author
+#         q.save()
+#         return q
+
+# class AnswerManager(models.Manager):
+#     def add_answer(self, text, question, author):
+#         a = super().create(text=text, question=question, author=author)
+#         question.answer = a
+#         question.save()
+#         return a
+    
 
 class Question(models.Model):
     topics = [
@@ -10,9 +172,9 @@ class Question(models.Model):
         ('Addition', '+'),
         ('Subtraction', '-'),
         ('Multiplication', '*'),
-        ('Divison', '/'),
-
+        ('Divison', '/')
     ]
+
     AGE_CHOICES = [
         ('3', '3 years old'),
         ('4', '4 years old'),
@@ -22,16 +184,14 @@ class Question(models.Model):
         ('8', '8 years old')
     ]
 
-    age = models.CharField(max_length=1, choices=AGE_CHOICES, default='3')  # Changed to CharField
+    age = models.CharField(max_length=1, choices=AGE_CHOICES, default='3')
     subject = models.CharField(max_length=200, choices=topics, default='General')
     question_text = models.CharField(max_length=200, default='')
-    answer_text = models.CharField(max_length=200, default='')
+    image = models.ImageField(upload_to='question_images/', default='')  # New field for the image
+    correct_answer = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.question_text
-
-    
-    from django.db import migrations, models
 
 def set_default_age(apps, schema_editor):
     Question = apps.get_model('eduprod', 'Question')
--- eduprod/views.py
+++ eduprod/views.py
@@ -1,16 +1,53 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, QuestionManager
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 @login_required(login_url='/accounts/signup')
 def home(request):
     selected_age = request.GET.get('age')  # Get the selected age from the request
+    # questions = Question.objects.all()
+    # questions= QuestionManager.questions
     return render(request, 'eduprod/home.html', {'selected_age': selected_age})
+    # return render(request, 'eduprod/home.html', {'questions': questions})
+
 
 @login_required(login_url='/accounts/signup')
+def get_questions2(request):
+    #topic = request.GET.get('topic')
+    # questions = Question.objects.filter(subject=topic)
+    # question_data = [{'question_text': question.question_text, 'image_url': question.image.url} for question in questions]
+    questions= QuestionManager.questions
+    return JsonResponse(questions, safe=False)
+
+
+#class UserQuestion:
+#    def __init__(self, id, questionText, answerChoice1, answerChoice2, answerChoice3, answerChoice4, answer):
+#        self.id = id
+#        self.questionText = questionText
+#        self.answerChoice1 = answerChoice1
+#       self.answerChoice2 = answerChoice2
+#       self.answerChoice3 = answerChoice3
+#       self.answerChoice4 = answerChoice4
+#       self.answer = answer
+
 def get_questions(request):
-    topic = request.GET.get('topic')
-    questions = Question.objects.filter(subject=topic)
-    data = [{'question_text': question.question_text} for question in questions]
-    return JsonResponse(data, safe=False)
+    questions = []
+
+    # Assuming you have a list of UserQuestion objects
+    # You need to serialize each object into a dictionary
+    # and then add it to the questions list
+    for user_question in QuestionManager.questions:
+        question_dict = {
+            'id': user_question.id,
+            'question_text': user_question.question_text,
+            'answer_choice1': user_question.answer_choice1,
+            'answer_choice2': user_question.answer_choice2,
+            'answer_choice3': user_question.answer_choice3,
+            'answer_choice4': user_question.answer_choice4,
+            'answer': user_question.answer
+        }
+        questions.append(question_dict)
+
+    # Finally, return the JsonResponse with the list of questions
+    return JsonResponse(questions, safe=False)
