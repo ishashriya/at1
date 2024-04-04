6810d58d3f0038325d084c416b854425acb04695
ishashriya <isha.shriya3@gmail.com>
2024-04-05 06:55:36 +1100

    version 2

--- eduprod/models.py
+++ eduprod/models.py
@@ -50,9 +50,25 @@ class QuestionManager:
             print("Returning questions for " + str(self.topic))
             self.questions = EarlyLearningQuestions.get_questions(self)
             return self.questions
+        elif self.topic == "Addition":
+            print("Returning questions for " + str(self.topic))
+            self.questions = AdditionQuestions.get_questions(self)
+            return self.questions
+        elif self.topic == "Subtraction":
+            print("Returning questions for " + str(self.topic))
+            self.questions = SubtractionQuestions.get_questions(self)
+            return self.questions
+        elif self.topic == "Multiplication":
+            print("Returning questions for " + str(self.topic))
+            self.questions = MultiplicationQuestions.get_questions(self)
+            return self.questions
+        elif self.topic == "Division":
+            print("Returning questions for " + str(self.topic))
+            self.questions = DivisionQuestions.get_questions(self)
+            return self.questions
         return self.questions
     
-    # Class representing the EarlyLearningQuestions
+# Class representing the EarlyLearningQuestions
 class EarlyLearningQuestions:
     def __init__(self):
         self.questions = []  # list of questions from early learning
@@ -69,25 +85,78 @@ class EarlyLearningQuestions:
             UserQuestion(7, "triangle", "circle", "triangle", "square", "oval", "triangle")
         ]
         return self.questions
-
-    def addEarlyLearningQuestion(self, question):
-        self.questions.append(question)
-
-    def question1(self):
-        q1 = UserQuestion(1, "Question 1 Square", "circle", "triangle", "square", "rectangle", "square") # Create a question with 5 choices
-        return q1
     
-    def question2(self):
-        q2 = UserQuestion(2, "Question 2 Circle", "circle", "triangle", "square", "rectangle", "circle") # Create a question with 5 choices
-        return q2
+# Class representing the AdditionQuestions
+class AdditionQuestions:
+    def __init__(self):
+        self.questions = []  # list of questions from addition
 
-    def question3(self):
-        q3 = UserQuestion(3, "Question 3 Rectangle", "circle", "triangle", "square", "rectangle", "rectangle") # Create a question with 5 choices
-        return q3
-
-
+    # Method to get addition questions
+    def get_questions(self):
+        self.questions = [
+            UserQuestion(1, "2 + 2", "1", "2", "5", "4", "4"),
+            UserQuestion(2, "3 + 2", "0", "2", "5", "4", "5"),
+            UserQuestion(3, "5 + 4", "9", "2", "5", "4", "9"),
+            UserQuestion(4, "4 + 3", "6", "7", "5", "4", "7"),
+            UserQuestion(5, "6 + 2", "5", "7", "8", "4", "8"),
+            UserQuestion(6, "8 + 3", "11", "7", "8", "4", "11"),
+            UserQuestion(7, "9 + 1", "11", "7", "8", "10", "10")
+        ]
+        return self.questions
     
-      
+# Class representing the SubstractionQuestions
+class SubtractionQuestions:
+    def __init__(self):
+        self.questions = []  # list of questions from subtraction
+
+    # Method to get subtraction questions
+    def get_questions(self):
+        self.questions = [
+            UserQuestion(1, "2 - 2", "1", "2", "5", "0", "0"),
+            UserQuestion(2, "3 - 2", "1", "2", "5", "4", "1"),
+            UserQuestion(3, "5 - 2", "3", "1", "5", "4", "3"),
+            UserQuestion(4, "4 - 2", "2", "7", "5", "4", "2"),
+            UserQuestion(5, "6 - 2", "5", "7", "8", "4", "4"),
+            UserQuestion(6, "8 - 3", "11", "5", "8", "4", "5"),
+            UserQuestion(7, "9 - 1", "11", "7", "8", "10", "8")
+        ]
+        return self.questions
+
+# Class representing the MultiplicationQuestions
+class MultiplicationQuestions:
+    def __init__(self):
+        self.questions = []  # list of questions from multiplication
+
+    # Method to get multiplication questions
+    def get_questions(self):
+        self.questions = [
+            UserQuestion(1, "2 x 2", "4", "2", "5", "0", "4"),
+            UserQuestion(2, "3 x 2", "1", "6", "5", "4", "6"),
+            UserQuestion(3, "5 x 2", "3", "10", "5", "4", "10"),
+            UserQuestion(4, "4 x 2", "8", "7", "5", "4", "8"),
+            UserQuestion(5, "6 x 2", "5", "7", "8", "12", "12"),
+            UserQuestion(6, "7 x 3", "11", "5", "21", "4", "21"),
+            UserQuestion(7, "9 x 1", "11", "9", "8", "10", "9")
+        ]
+        return self.questions
+    
+# Class representing the DivisionQuestions
+class DivisionQuestions:
+    def __init__(self):
+        self.questions = []  # list of questions from division
+
+    # Method to get division questions
+    def get_questions(self):
+        self.questions = [
+            UserQuestion(1, "2 / 2", "4", "2", "5", "1", "1"),
+            UserQuestion(2, "6 / 2", "3", "6", "5", "4", "3"),
+            UserQuestion(3, "9 / 3", "3", "10", "5", "4", "3"),
+            UserQuestion(4, "4 / 2", "8", "7", "2", "4", "2"),
+            UserQuestion(5, "8 / 2", "5", "4", "8", "12", "4"),
+            UserQuestion(6, "10 / 5", "11", "5", "2", "4", "2"),
+            UserQuestion(7, "1 / 1", "1", "9", "8", "10", "1")
+        ]
+        return self.questions      
 
 class AnswerManager:
     def __init__(self):
