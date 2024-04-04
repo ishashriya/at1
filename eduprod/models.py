ff5be573b437d0ce1e6cca982befe6c5db7b039d
ishashriya <isha.shriya3@gmail.com>
2024-04-05 01:19:11 +1100

    version 1.8.3

--- eduprod/models.py
+++ eduprod/models.py
@@ -4,114 +4,6 @@ from django.utils.translation import gettext_lazy as _
 from django.db import migrations, models
 from django.core import serializers
 
-# Class representing the AnswerChoice
-class AnswerChoice:
-    def __init__(self, answerChoice1, answerChoice2, answerChoice3, answerChoice4):
-        self.answerChoice1 = answerChoice1
-        self.answerChoice2 = answerChoice2
-        self.answerChoice3 = answerChoice3
-        self.answerChoice4 = answerChoice4
-
-        # Getter and setter methods for answer choices
-        @property
-        def answerChoice1(self):
-            return self.answerChoice1
-        
-        @answerChoice1.setter
-        def answerChoice1(self, value):
-            self.answerChoice1 = value
-
-        @property
-        def answerChoice2(self):
-            return self.answerChoice2
-        
-        @answerChoice2.setter
-        def answerChoice2(self, value):
-            self.answerChoice2 = value
-
-        @property
-        def answerChoice3(self):
-            return self.answerChoice3
-        
-        @answerChoice3.setter
-        def answerChoice3(self, value):
-            self.answerChoice3 = value
-
-        @property
-        def answerChoice4(self):
-            return self.answerChoice4
-        
-        @answerChoice4.setter
-        def answerChoice4(self, value):
-            self.answerChoice4 = value
-        
-# Class representing the UserQuestion1
-class UserQuestion1:
-    def __init__(self, id, questionText, answerChoice1, answerChoice2, answerChoice3, answerChoice4, answer):
-        self.id = id
-        self.questionText = questionText
-        self.answerChoice1 = answerChoice1
-        self.answerChoice2 = answerChoice2
-        self.answerChoice3 = answerChoice3
-        self.answerChoice4 = answerChoice4
-        self.answer = answer
-        
-        # Getter and setter methods for question and answer choices
-        @property
-        def id(self):
-            return self.id
-        
-        @id.setter
-        def id(self, value):
-            self.id = value
-
-        @property
-        def questionText(self):
-            return self.questionText
-        
-        @questionText.setter
-        def questionText(self, value):
-            self.questionText = value  
-
-        @property
-        def answerChoice1(self):
-            return self.answerChoice1
-        
-        @answerChoice1.setter
-        def answerChoice1(self, value):
-            self.answerChoice1 = value
-
-        @property
-        def answerChoice2(self):
-            return self.answerChoice2
-        
-        @answerChoice2.setter
-        def answerChoice2(self, value):
-            self.answerChoice2 = value
-
-        @property
-        def answerChoice3(self):
-            return self.answerChoice3
-        
-        @answerChoice3.setter
-        def answerChoice3(self, value):
-            self.answerChoice3 = value
-
-        @property
-        def answerChoice4(self):
-            return self.answerChoice4
-        
-        @answerChoice4.setter
-        def answerChoice4(self, value):
-            self.answerChoice4 = value
-
-        @property
-        def answer(self):
-            return self.answer
-        
-        @answer.setter
-        def answer(self, value):
-            self.answer = value
 
 # Model representing the UserAnswer
 class UserAnswer(models.Model):
@@ -167,14 +59,14 @@ class EarlyLearningQuestions:
 
     # Method to get early learning questions
     def get_questions(self):
-        #self.addEarlyLearningQuestion(self.question1())
-        #self.addEarlyLearningQuestion(self.question2())
-        #self.addEarlyLearningQuestion(self.question3())
-        #self.questions = [self.question1(self), self.question2(), self.question3()]
         self.questions = [
-            UserQuestion(1, "Question 1 Square", "circle", "triangle", "square", "rectangle", "square"),
-            UserQuestion(2, "Question 2 Circle", "circle", "triangle", "square", "rectangle", "circle"),
-            UserQuestion(3, "Question 3 Rectangle", "circle", "triangle", "square", "rectangle", "rectangle")
+            UserQuestion(1, "square", "circle", "triangle", "square", "rectangle", "square"),
+            UserQuestion(2, "circle", "circle", "triangle", "square", "rectangle", "circle"),
+            UserQuestion(3, "rectangle", "circle", "triangle", "square", "rectangle", "rectangle"),
+            UserQuestion(4, "oval", "circle", "triangle", "square", "oval", "oval"),
+            UserQuestion(5, "trapezoid", "trapezoid", "rectangle", "square", "oval", "trapezoid"),
+            UserQuestion(6, "parallelogram", "rectangle", "parallelogram", "square", "oval", "parallelogram"),
+            UserQuestion(7, "triangle", "circle", "triangle", "square", "oval", "triangle")
         ]
         return self.questions
 
